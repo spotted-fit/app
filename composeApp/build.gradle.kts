55031355b78b1231616c0282154023781cba4d1e
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2025-04-28 18:39:17 +0200

    CORS issue

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -88,7 +88,6 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.client.logging)
-            implementation(libs.ktor.client.cors)
             implementation(libs.kotlinx.serialization.json)
 
             // Emoji support
